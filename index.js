04cde269544caac57838c76636e18594c834426c
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-23 13:49:14 +0530

    Updated App

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 var port=process.env.PORT || 8080;
 
+var posts=["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."];
+var postTitles=["First Blog Post","Second Blog Post"];
 // const homeContent;
 // const aboutContent;
 
@@ -19,10 +21,12 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Static Files Served
-app.use(express.static('public'));
+app.use('/public',express.static('public'));
 
-
-
+// Home Route
+app.get('/',(req,res)=>{
+    res.render("home.ejs",{posts:posts,title:postTitles});
+});
 
 
 app.listen(port, ()=>{
